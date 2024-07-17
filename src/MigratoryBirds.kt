639bd92585a59fd4edede7e050ef40fd2b743116
ramesh <pokhrelramesh1996@gmail.com>
2024-07-17 12:05:14 +0545

    update kt files

--- src/MigratoryBirds.kt
+++ src/MigratoryBirds.kt
@@ -1,22 +1,8 @@
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
+
 import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
 import kotlin.collections.*
-import kotlin.comparisons.*
 import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.math.max
 import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
 import java.util.HashMap
 import java.util.Map
 
--- src/problem_solving/getTotalX.kt
+++ src/problem_solving/getTotalX.kt
@@ -1,25 +1,7 @@
 package problem_solving
 
-import sun.java2d.cmm.lcms.LCMS
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
-import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
-import kotlin.collections.*
-import kotlin.comparisons.*
 import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
 import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
-
 /*
  * Complete the 'getTotalX' function below.
  *
